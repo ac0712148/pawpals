8bfeddd1a62be5c6a497679b18614ae365abafed
Mark Prodehl <markprodehl@gmail.com>
2020-05-16 20:37:35 -0700

    added LandingPage

---
+++ client/src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,25 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+// import "./LandingPage.css";
+// import NavLinks from "../../components/Navbar/Navbar"
+
+function LandingPage() {
+
+    return (
+        <div className="container text-center">
+            <div className="bg1">
+                <h2><i className="fas fa-dog fa-2x" /></h2>
+                <p>Landing Page</p>
+                
+                <button type="button" class="btn btn-primary btn-lg">Signup</button>
+                <button type="button" class="btn btn-secondary btn-lg m-3">Login</button>
+            </div>
+
+
+
+        </div>
+
+    )
+}
+
+export default LandingPage;
