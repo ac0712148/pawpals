71444e81ed0573aaf81493b33a2905bab6232600
Mark Prodehl <markprodehl@gmail.com>
2020-05-16 22:35:42 -0700

    added links to profile, photos, and my posts to Following page

--- client/src/pages/Following/Following.js
+++ client/src/pages/Following/Following.js
@@ -10,9 +10,15 @@ function Following() {
                 <h1 className="display-4">Following</h1>
                 
                 <p className="lead mt-5">Contrary to popular beliet. It has roots in a piece of classNameical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classNameical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-                <button type="button" className="btn btn-success btn-lg">
-                    <Link className="text-white" to="/signup">Signup</Link>
-                </button>
+                <button type="button" className="btn btn-info btn-sm">
+      <Link to="/profile">Back To Profile</Link>
+      </button>
+      <button type="button" className="btn btn-info btn-sm ml-5">
+      <Link to="/myphotos">My Photos</Link>
+      </button>
+      <button type="button" className="btn btn-info btn-sm ml-5">
+      <Link to="/posts">My Posts</Link>
+      </button>
             </div>
         </div>
         
