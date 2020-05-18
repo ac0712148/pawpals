7a68464aae5f38d5f00afaff75a1880a0021b85d
Albert Cervantes <cervanalbert@gmail.com>
2020-05-18 01:17:14 -0700

    added patch route

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require('../models');
+const { ObjectID } = require('mongodb');
 
 const router = express.Router();
 
@@ -33,7 +34,7 @@ router.post('/api/post', (req, res) => {
 });
 
 // Delete route deletes the post of a specified user
-router.delete('/api/:id', (req,res) => {
+router.delete('/api/post/:id', (req,res) => {
     db.Post.findById(req.params.id)
     .then((post) => {
         post.remove();
@@ -46,6 +47,148 @@ router.delete('/api/:id', (req,res) => {
 // Patch route to update posts
 // The following things can happend for when updating
     // Only the owner can update their post
-    // 
+    // Update based on a specific action:
+        // Could be adding likes
+        // adding comments
+        // editing
+router.patch('/api/post/:id', (req, res) => {
+    // id is the _id of the post
+    const { id } = req.params;
+    if(!ObjectID.isValid(id)) {
+        res.status(404).send('Invalid id');
+    }
+    // For add Comment, we need:
+    // {                    CommenterId is the user posting comment
+    //     "commenterId": "5ec2281fa0b9cf23e0049cd1",
+    //     "text": "this is sample text"
+    //     "updateAction": "addComment"
+    //  }
+    else if(req.body.updateAction === 'addComment') {
+        console.log(req.body);
+        db.Post.findByIdAndUpdate(
+            id,
+            {
+              $push: {
+                comments: {
+                  commenterId: req.body.commenterId,
+                  text: req.body.text,
+                  timestamp: new Date().getTime()
+                }
+              }
+            },
+            { new: true },
+            (err, post) => {
+              if (err) {
+                  res.status(400).send(err);
+              }
+              res.send(post);
+            }
+        );
+    }
+    // For delete comment, we need:
+    // {                Comment id is the id of the comment
+    //     "commentId": "5ec2281fa0b9cf23e0049cd1",
+    //     "updateAction": "deleteComment"
+    //  }
+    else if(req.body.updateAction === 'deleteComment') {
+        db.Post.findByIdAndUpdate(
+            id,
+            {
+              $pull: {
+                comments: {
+                  _id: req.body.commentId
+                }
+              }
+            },
+            { new: true },
+            (err, post) => {
+              if (err) {
+                  res.status(400).send(err);
+              }
+              res.send(post);
+            }
+        );
+    }
+    // {
+    //     "commentId": "5ec22875b7a0e036d8d628a6",
+    //     "text": "this is a comment",
+    //     "updateAction": "editComment"
+    // }
+    else if(req.body.updateAction === 'editComment') {
+        db.Post.findById(id, (err, post) => {
+            const { comments } = post;
+            const theComment = comments.find(comment => 
+                comment._id.equals(req.body.commentId)
+            );
+            if (!theComment) {
+                res.status(404).send('Unable to find comment');
+            }
+            theComment.text = req.body.text;
+    
+            post.save((err) => {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                res.status(200).send(post);
+            });
+        });
+    }
+    // {        id is the user liking the post
+    //     "id": "5ec22c48247b5f22882fee04",
+    //     "updateAction": "like"
+    // }
+    else if(req.body.updateAction === "like") {
+        db.Post.findByIdAndUpdate(
+            id,
+            {
+                $inc: { likesCount: 1 },
+                $addToSet: { likers: req.body.id }
+            },
+            { new: true },
+            (err, post) => {
+                if (err) {
+                    res.status(400).send(err)
+                };
+                res.send(post);
+            }
+        );
+    }
+    // {
+    //     "id": "5ec22c48247b5f22882fee04",
+    //     "updateAction": "unlike"
+    // }
+    else if(req.body.updateAction === "unlike") {
+        db.Post.findByIdAndUpdate(
+            id,
+            {
+                $inc: { likesCount: -1 },
+                $pull: { likers: req.body.id }
+            },
+            { new: true },
+            (err, post) => {
+                if (err) {
+                    res.status(400).send(err)
+                };
+                res.send(post);
+            }
+        );
+    }
+    // {
+    //     "text": "We are editing the post"
+    // }
+    else {
+        db.Post.findByIdAndUpdate(
+            id,
+            { $set: { text: req.body.text } },
+            { new: true },
+            (err, post) => {
+                if (err) {
+                    res.status(400).send(err)
+                };
+                res.send(post);
+            }
+        );
+    }
+});
 
 module.exports = router;
