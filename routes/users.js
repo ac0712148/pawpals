d3af486b60b38ab327dda371b3febfce4e617aff
michaelleojacob <michaelleojacob@gmail.com>
2020-05-20 11:38:10 -0700

    i think i finished the user routes for now. believe the tests on postman all worked

--- routes/users.js
+++ routes/users.js
@@ -70,4 +70,76 @@ router.patch("/api/user/:id", isAuthenticated, (req, res) => {
     });
 });
 
+//get all followers
+router.get("/api/followers", async (req, res) => {
+  const users = await db.User.find();
+  res.status(200).json(users);
+});
+
+//add a user to the list of users that are following our user
+router.patch("/api/followers/:id", isAuthenticated, (req, res) => {
+  //making sure the user is logged in and is the ONE user
+  const { id } = req.params;
+  if (req.user.id !== id) {
+    return res.sendStatus(401);
+  }
+  //if the user is logged in, find and update. add to set: followers by their ID
+  db.User.findOneAndUpdate(
+    { _id: id },
+    {
+      $addToSet: { followers: id },
+    },
+    { new: true, upsert: true, setDefaultsOnInsert: true },
+    (err) => {
+      if (err !== null && err.name === "MongoError" && err.code === 11000) {
+        return res.status(500).send({ message: "something went wrong." });
+      }
+    }
+  )
+    .then((user) => {
+      return res.json(user);
+    })
+    .catch((err) => {
+      if (err !== null && err.name === "MongoError" && err.code === 11000) {
+        return res
+          .status(500)
+          .send({ message: "This email is already in use." });
+      }
+      console.log(err);
+      return res.status(500).json({ message: err });
+    });
+});
+
+//unfollow
+router.patch("/api/unfollowers/:id", isAuthenticated, (req, res) => {
+  const { id } = req.params;
+  if (req.user.id !== id) {
+    return res.sendStatus(401);
+  }
+  db.User.findOneAndUpdate(
+    { _id: id },
+    {
+      $pull: { followers: id },
+    },
+    { new: true, upsert: true, setDefaultsOnInsert: true },
+    (err) => {
+      if (err !== null && err.name === "MongoError" && err.code === 11000) {
+        return res.status(500).send({ message: "something went wrong." });
+      }
+    }
+  )
+    .then((user) => {
+      return res.json(user);
+    })
+    .catch((err) => {
+      if (err !== null && err.name === "MongoError" && err.code === 11000) {
+        return res
+          .status(500)
+          .send({ message: "This email is already in use." });
+      }
+      console.log(err);
+      return res.status(500).json({ message: err });
+    });
+});
+
 module.exports = router;
