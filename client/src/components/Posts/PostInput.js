8fb19e595b79663918134536af1595ca1f215cf0
Albert Cervantes <cervanalbert@gmail.com>
2020-05-21 02:42:41 -0700

    changed class to functional component

--- client/src/components/Posts/PostInput.js
+++ client/src/components/Posts/PostInput.js
@@ -1,44 +1,56 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button'
+import Button from '@material-ui/core/Button';
+import { useAuth } from "../../utils/auth";
+import axios from "axios";
 
-class PostInput extends Component {
-    // state =
+export default function PostInput() {
+    const { user } = useAuth();
+    const [textFieldValue,
+        setTextFieldValue] = useState("");
 
-    // handleClick
+    const handleTextFieldChange = (e) => {
+        setTextFieldValue(e.target.value);
+    };
 
-    render() {
-        return (
-            <form
-                className="container"
+    const handleSubmit = () => {
+        // setTextFieldValue(e.target.value);
+        console.log("On click value: " + textFieldValue);
+        axios.post('/api/post', {
+            authorId: user.id,
+            text: textFieldValue
+        })
+    };
+
+    return (
+        <form
+            className="container"
+            style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: '25px',
+            marginBottom: '25px'
+        }}
+            noValidate
+            autoComplete="off">
+            <TextField
+                value={textFieldValue}
+                onChange={handleTextFieldChange}
+                id="standard-basic"
                 style={{
-                display: "flex",
-                justifyContent: "center",
-                marginTop: '25px',
-                marginBottom: '25px'
+                width: "90%",
+                height: "45px"
             }}
-                noValidate
-                autoComplete="off">
-                <TextField
-                    id="standard-basic"
-                    style={{
-                    width: "90%",
-                    height: "45px",
-                    // paddingBottom: "5px"
-                }}
-                    label="PawPost!..."/>
-                <Button
-                    variant="contained"
-                    color="primary"
-                    style={{
-                    margin: "3px",
-                    // height: "25px"
-                }}>
-                    <i className="fas fa-angle-right"></i>
-                </Button>
-            </form>
-        );
-    }
+                label="PawPost!..."/>
+            <Button
+                onClick={handleSubmit}
+                variant="contained"
+                color="primary"
+                style={{
+                margin: "3px"
+            }}>
+                <i className="fas fa-angle-right"></i>
+            </Button>
+        </form>
+    )
 }
-
-export default PostInput;
