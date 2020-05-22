734fc2f78dabaccd003bc4770555af398d1a8eb4
Albert Cervantes <cervanalbert@gmail.com>
2020-05-22 02:09:12 -0700

    removed use state, not used

--- client/src/components/Posts/PostInput.js
+++ client/src/components/Posts/PostInput.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 // import { useAuth } from "../../utils/auth";
