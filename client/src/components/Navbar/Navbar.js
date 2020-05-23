2d1234263c72c654bbb6daca14b677c6efc3d6bf
Mark Prodehl <markprodehl@gmail.com>
2020-05-23 13:05:59 -0700

    Navbar styling

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,125 +2,81 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../utils/auth";
 
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import "./NavBar.css"
 
 const createLink = ({ text, to, ...rest }) => {
-  const className = "nav-link";
+  const className = "topnav";
+  const id = "myTopnav"
   if (to) {
     return (
-      <Link className={className} to={to} {...rest}>
+      <Link className={className} id={id} to={to} {...rest}>
         {text}
       </Link>
     );
   }
   return (
-    <span
+    <a
       role="button"
-      className={className}
+      className="topnav logout"
+      id={id}
       style={{ cursor: "pointer" }}
       {...rest}
     >
       {text}
-    </span>
+    </a>
   );
 };
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
 
 function NavLinks() {
   const { isLoggedIn, logout } = useAuth();
   const links = [];
   if (isLoggedIn) {
-    links.push({ text: "Profile", to: "/newprofile" });
-    links.push({ text: "Logout", onClick: () => logout() });
+    links.push({ text: "My Profile", to: "/newprofile" });
     links.push({ text: "Following", to: "/following" });
-    links.push({ text: "MyPhotos", to: "/myphotos" });
-    links.push({ text: "Forum", to: "/newprofile" });
-    
+    links.push({ text: "Photos", to: "/myPhotos" });
+    links.push({ text: "Forum", to: "/posts" });
+    links.push({ text: "Logout", onClick: () => logout() });
   } else {
     links.push({ text: "Signup", to: "/signup" });
     links.push({ text: "Login", to: "/login" });
+    // links.push({ text: "Home", to: "/landingpage" });
   }
   return (
-    <ul className="navbar-nav">
+    <div className="topnav" id="myTopnav">
       {links.map((link, i) => (
-        <li key={i} className="nav-item">
+        <span href="/" key={i} className="">
           {createLink(link)}
-        </li>
+        </span>
       ))}
-    </ul>
+      <a href="#/" className="icon" onClick={myFunction}>
+        <i className="fa fa-bars" />
+      </a>
+      
+    </div>
   );
+  
+  
+
 }
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
 function Navbar() {
-  const classes = useStyles();
-
-  const { isLoggedIn, logout } = useAuth();
-  const links = [];
-  if (isLoggedIn) {
-    links.push({ text: "Profile", to: "/newprofile" });
-    links.push({ text: "Logout", onClick: () => logout() });
-    links.push({ text: "Following", to: "/following" });
-    links.push({ text: "MyPhotos", to: "/myphotos" });
-    links.push({ text: "Forum", to: "/posts" });
-    
-  } else {
-    links.push({ text: "Signup", to: "/signup" });
-    links.push({ text: "Login", to: "/login" });
-  }
-
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-        {links.map((link, i) => (
-        <div key={i} className="nav-item">
-          {createLink(link)}
-        </div>
-      ))}
-          
-        </Toolbar>
-      </AppBar>
+    <div className="topnav" id="myTopnav">
+      <NavLinks />
+      
+      {/* <a href="#/" className="icon" onClick={myFunction}>
+        <i className="fa fa-bars" />
+      </a> */}
     </div>
-
-    // <nav className="navbar navbar-expand navbar-dark">
-    //   <div className="container navbar-container">
-    //     <Link className="navbar-nav nav-item text-white" to="/profile">
-    //       Paw Pals (old profile)
-    //       <i className="fas fa-dog fa-2x" />
-    //     </Link>
-    //     <Link className="text-white" to="/newprofile">
-    //       NewProfile       
-    //     </Link>
-    //     <Link className="text-white" to="/following">
-    //       Following       
-    //     </Link>
-    //     <Link className="text-white" to="/myphotos">
-    //       Photos      
-    //     </Link>
-    //     <Link className="text-white" to="/posts">
-    //       Posts       
-    //     </Link>
-        
-    //     <NavLinks />
-    //   </div>
-    // </nav>
   );
 }
 
