677c4f4fff20aa4ed49865ad1d704be57b9215a0
Mark Prodehl <markprodehl@gmail.com>
2020-05-22 22:15:48 -0700

    new nav layout

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,6 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../utils/auth";
 
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
 const createLink = ({ text, to, ...rest }) => {
   const className = "nav-link";
   if (to) {
@@ -29,6 +37,10 @@ function NavLinks() {
   if (isLoggedIn) {
     links.push({ text: "Profile", to: "/newprofile" });
     links.push({ text: "Logout", onClick: () => logout() });
+    links.push({ text: "Following", to: "/following" });
+    links.push({ text: "MyPhotos", to: "/myphotos" });
+    links.push({ text: "Forum", to: "/newprofile" });
+    
   } else {
     links.push({ text: "Signup", to: "/signup" });
     links.push({ text: "Login", to: "/login" });
@@ -44,30 +56,71 @@ function NavLinks() {
   );
 }
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 function Navbar() {
+  const classes = useStyles();
+
+  const { isLoggedIn, logout } = useAuth();
+  const links = [];
+  if (isLoggedIn) {
+    links.push({ text: "Profile", to: "/newprofile" });
+    links.push({ text: "Logout", onClick: () => logout() });
+    links.push({ text: "Following", to: "/following" });
+    links.push({ text: "MyPhotos", to: "/myphotos" });
+    links.push({ text: "Forum", to: "/posts" });
+    
+  } else {
+    links.push({ text: "Signup", to: "/signup" });
+    links.push({ text: "Login", to: "/login" });
+  }
+
   return (
-    <nav className="navbar navbar-expand navbar-dark">
-      <div className="container navbar-container">
-        <Link className="navbar-nav nav-item text-white" to="/profile">
-          Paw Pals (old profile)
-          <i className="fas fa-dog fa-2x" />
-        </Link>
-        <Link className="text-white" to="/newprofile">
-          NewProfile       
-        </Link>
-        <Link className="text-white" to="/following">
-          Following       
-        </Link>
-        <Link className="text-white" to="/myphotos">
-          Photos      
-        </Link>
-        <Link className="text-white" to="/posts">
-          Posts       
-        </Link>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+        {links.map((link, i) => (
+        <div key={i} className="nav-item">
+          {createLink(link)}
+        </div>
+      ))}
+          
+        </Toolbar>
+      </AppBar>
+    </div>
+
+    // <nav className="navbar navbar-expand navbar-dark">
+    //   <div className="container navbar-container">
+    //     <Link className="navbar-nav nav-item text-white" to="/profile">
+    //       Paw Pals (old profile)
+    //       <i className="fas fa-dog fa-2x" />
+    //     </Link>
+    //     <Link className="text-white" to="/newprofile">
+    //       NewProfile       
+    //     </Link>
+    //     <Link className="text-white" to="/following">
+    //       Following       
+    //     </Link>
+    //     <Link className="text-white" to="/myphotos">
+    //       Photos      
+    //     </Link>
+    //     <Link className="text-white" to="/posts">
+    //       Posts       
+    //     </Link>
         
-        <NavLinks />
-      </div>
-    </nav>
+    //     <NavLinks />
+    //   </div>
+    // </nav>
   );
 }
 
