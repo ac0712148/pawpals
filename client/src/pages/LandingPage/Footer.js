b88043006873bbed6ed9fb31a8583ba5d99bb73b
michaelleojacob <michaelleojacob@gmail.com>
2020-05-23 18:46:02 -0700

    fixed up an issue with the footer. still deciding if i should change it or not

--- client/src/pages/LandingPage/Footer.js
+++ client/src/pages/LandingPage/Footer.js
@@ -3,15 +3,15 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
+// import Link from '@material-ui/core/Link';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
+      {/* <Link color="inherit" href="https://material-ui.com/"> */}
         Pawpals
-      </Link>{' '}
+      {/* </Link>{' '} */}
       {new Date().getFullYear()}
       {'.'}
     </Typography>
